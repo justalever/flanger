6cf158afc6ed326df4ae7005d618271676580b3c
Luis  Senior <heybluguy@yahoo.com>
2019-05-10 16:45:30 -0500

    Refactored the cart items counter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,10 @@
 module ApplicationHelper
-
   def cart_count_over_one
-    if total_cart_items > 0
-      return "<span class='tag is-dark'>#{total_cart_items}</span>".html_safe
-    end
+    return total_cart_items if total_cart_items > 0
   end
 
   def total_cart_items
-     total = @cart.line_items.map { |item| item.quantity }.sum
-     return total if total > 0
+    total = @cart.line_items.map(&:quantity).sum
+    return total if total > 0
   end
 end
